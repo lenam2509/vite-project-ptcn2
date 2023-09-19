b7d5973784f3092ec29dc8964d78bcf427c9c7c9
Le Viet Nam <levietnam2509@gmail.com>
2023-09-19 17:18:30 +0700

    allmost done container 3

--- client/src/pages/client/Home.jsx
+++ client/src/pages/client/Home.jsx
@@ -26,6 +26,32 @@ export default function Home() {
           <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
         </div>
       </div>
+      <div className="container3">
+        <h1>Danh mục nổi bật</h1>
+        <div className="box_container">
+          <div className="box">
+            <img
+              src="https://images.unsplash.com/photo-1694681733456-05c706f280b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+            <div className="content">Lorem</div>
+          </div>
+          <div className="box">
+            <img
+              src="https://images.unsplash.com/photo-1694681733456-05c706f280b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+            <div className="content">Lorem</div>
+          </div>
+          <div className="box">
+            <img
+              src="https://images.unsplash.com/photo-1694681733456-05c706f280b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8eGpQUjRobGtCR0F8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+            <div className="content">Lorem</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
