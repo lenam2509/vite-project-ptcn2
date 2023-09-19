ff3d9adb5608541264071dcb0bccf446a8e0a6ec
Le Viet Nam <levietnam2509@gmail.com>
2023-09-19 16:55:48 +0700

    done container2

--- client/src/pages/client/Home.jsx
+++ client/src/pages/client/Home.jsx
@@ -1,9 +1,31 @@
 import SlideBaner from "../../components/SlideBaner";
-
+import "../../styles/home.css";
 export default function Home() {
   return (
-    <div>
+    <>
       <SlideBaner />
-    </div>
+      <div className="container2">
+        <div className="row">
+          <h1>Logo</h1>
+          <h2>Lorem Ipsum</h2>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+        </div>
+        <div className="row">
+          <h1>Logo</h1>
+          <h2>Lorem Ipsum</h2>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+        </div>
+        <div className="row">
+          <h1>Logo</h1>
+          <h2>Lorem Ipsum</h2>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+        </div>
+        <div className="row">
+          <h1>Logo</h1>
+          <h2>Lorem Ipsum</h2>
+          <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</span>
+        </div>
+      </div>
+    </>
   );
 }
