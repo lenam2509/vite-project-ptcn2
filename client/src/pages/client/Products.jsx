13508e163ebe775b509e233c8b42da37b4a65b34
Le Viet Nam <levietnam2509@gmail.com>
2023-09-20 18:57:13 +0700

    done trang sản phẩm

--- client/src/pages/client/Products.jsx
+++ client/src/pages/client/Products.jsx
@@ -1,6 +1,56 @@
-
+import ProductCard from "../../components/ProductCard";
+import "../../styles/productspage.css";
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <div>
+      <div className="proucts_query">
+        <div className="products_selections">
+          <p>Thể loại</p>
+          <select>
+            <option value="all">Tất cả</option>
+            <option value="1">Thể loại 1</option>
+            <option value="2">Thể loại 2</option>
+            <option value="3">Thể loại 3</option>
+          </select>
+        </div>
+        <div className="products_searcher">
+          <input type="text" placeholder="tìm sản phẩm" />
+          <button>Search</button>
+        </div>
+        <div className="products_sorter">
+          <p>Sắp xếp theo</p>
+          <select>
+            <option value="1">Giá tăng dần</option>
+            <option value="2">Giá giảm dần</option>
+            <option value="3">Tên A-Z</option>
+            <option value="4">Tên Z-A</option>
+          </select>
+        </div>
+      </div>
+
+      {/* <div className="products_selections">
+        <div className="products_selections_item">
+          <p>Thể loại</p>
+          <select>
+            <option value="all">Tất cả</option>
+            <option value="1">Thể loại 1</option>
+            <option value="2">Thể loại 2</option>
+            <option value="3">Thể loại 3</option>
+          </select>
+        </div>
+      </div> */}
+      <div className="products_container">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </div>
+  );
 }
