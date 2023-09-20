e6ece18f805d8fa6953167ed4702a106a459fe8c
Le Viet Nam <levietnam2509@gmail.com>
2023-09-21 02:54:19 +0700

    done detail

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaBars } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
 import { useRef } from "react";
+import Product from "../assets/images/product.jpg";
 export default function Navbar() {
   const NavRef = useRef();
   const CartRef = useRef();
@@ -55,17 +56,17 @@ export default function Navbar() {
         <div className="cart_detail" ref={CartRef}>
           <h2>Giỏ hàng</h2>
           <div className="cart_item">
-            <img src="https://picsum.photos/200/300" alt="" />
+            <img src={Product} alt="" />
             <div className="cart_item_detail">
-              <h3>Trà sữa</h3>
-              <p>100.000</p>
-              <p>x5</p>
+              <h3>Trà vải</h3>
+              <p className="price">100.000đ</p>
+              <p className="quantity">x5</p>
             </div>
             <BiSolidTrashAlt />
           </div>
 
           <div className="cart_total_money">
-            <p>Tổng tiền:</p> <span>100.000vnđ</span>
+            <p>Tổng tiền:</p> <span>100.000đ</span>
           </div>
           <div className="cart_btn">
             <button className="cart_btn_buy">Xem giỏ hàng</button>
--- client/src/pages/client/ProductDetail.jsx
+++ client/src/pages/client/ProductDetail.jsx
@@ -1,5 +1,57 @@
 import "../../styles/productdetail.css";
-
+import Product from "../../assets/images/product.jpg";
+import { BsCartPlusFill } from "react-icons/bs";
+import { Fragment } from "react";
+import ProductCard from "../../components/ProductCard";
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  return (
+    <Fragment>
+      <div className="detail_container">
+        <div className="detail_imgs">
+          <div className="detail_img">
+            <img src={Product} alt="" />
+          </div>
+          <div className="detail_small_img">
+            <img src={Product} alt="" />
+            <img src={Product} alt="" />
+            <img src={Product} alt="" />
+          </div>
+        </div>
+
+        <div className="detail_content">
+          <h1>Trà vải</h1>
+          <span className="price">20.000đ</span>
+          <p>
+            Và trà vải cũng thế nó là một loại trà trái cây tự nhiên có trà, quả
+            vải thiều ngâm, mật ong, nước lạnh. Một số nơi còn biến tấu món trà
+            này thành trà cùng nước ép trái cây, chiết xuất của quả vải,… để dễ
+            uống hơn vưới những người không thích vị đậm của trà.
+          </p>
+          <div className="detail_quantity">
+            <button className="btn_desc">-</button>
+            <span>1</span>
+            <button className="btn_inc">+</button>
+          </div>
+          <div className="detail_btn">
+            <button>
+              Thêm <BsCartPlusFill />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="same_products">
+        <div className="title">
+          <div className="breakrum"></div>
+          <h1>Sản phẩm liên quan</h1>
+          <div className="breakrum"></div>
+        </div>
+        <div className="same_products_container">
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
+      </div>
+    </Fragment>
+  );
 }
