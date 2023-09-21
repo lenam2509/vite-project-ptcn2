2238e92e5f837bdccb1984d9bd03f9f9ef03cfe8
Le Viet Nam <levietnam2509@gmail.com>
2023-09-21 15:21:40 +0700

    fix baner

--- client/src/components/SlideBaner.jsx
+++ client/src/components/SlideBaner.jsx
@@ -4,7 +4,7 @@ import "../styles/slidebaner.css";
 import baner1 from "../assets/images/baner1.jpg";
 import baner2 from "../assets/images/baner2.jpg";
 export default function SlideBaner() {
-  const images = [baner1, baner2];
+  const images = [baner2, baner1];
   return (
     <Slide>
       {images.map((image, index) => (
