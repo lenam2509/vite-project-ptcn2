5e5d3fd3d6ad0aeb9cd24f35c463550ca809bb1a
Le Viet Nam <levietnam2509@gmail.com>
2023-09-24 23:31:07 +0700

    done admin layout

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -1,3 +1,22 @@
+import "../styles/topbar.css";
+import { BiUser, BiBell } from "react-icons/bi";
+import { FiSettings } from "react-icons/fi";
+
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="topbar">
+      <div className="topbar_logo">Logo</div>
+      <div className="topbar_icons">
+        <div className="topbar_icon_item">
+          <BiUser />
+        </div>
+        <div className="topbar_icon_item">
+          <BiBell />
+        </div>
+        <div className="topbar_icon_item">
+          <FiSettings />
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/admin/AdminRoute.jsx
+++ client/src/pages/admin/AdminRoute.jsx
@@ -1,14 +1,26 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Topbar from "../../components/Topbar";
+import Sidebar from "../../components/Sidebar";
+import Products from "./Products";
+import Bills from "./Bills";
+import Users from "./Users";
 
 export default function AdminRoute() {
   return (
     <Router>
       <Topbar />
-      <Switch>
-        <Route path="/admin" component={Dashboard} />
-      </Switch>
+      <div style={{ display: "flex" }}>
+        <Sidebar />
+        <div style={{ padding: "1rem", width: "100%" }}>
+          <Switch>
+            <Route path="/admin/dashboard" exact component={Dashboard} />
+            <Route path="/admin/products" exact component={Products} />
+            <Route path="/admin/bills" exact component={Bills} />
+            <Route path="/admin/users" exact component={Users} />
+          </Switch>
+        </div>
+      </div>
     </Router>
   );
 }
