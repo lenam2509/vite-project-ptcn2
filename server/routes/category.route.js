8feab5a0bfaecc30a96b67ec490e93fa746b9129
Le Viet Nam <levietnam2509@gmail.com>
2023-10-15 19:25:10 +0700

    remove server

--- server/routes/category.route.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controllers/category.controller');
-
-router.post('/', categoryController.addCategory);
-router.get('/', categoryController.getCategories);
-
-module.exports = router;
